15b8eb39c15949af881cb639bc2f9433d7493424
hanks <zhouhan315@gmail.com>
2015-07-22 00:17:31 +0900

    lab1-part2

--- src/mapreduce/master.go
+++ src/mapreduce/master.go
@@ -28,6 +28,42 @@ func (mr *MapReduce) KillWorkers() *list.List {
 
 func (mr *MapReduce) RunMaster() *list.List {
 	// Your code here
+	_ = "breakpoint"
+	var workerAddr string
+
+	for i := 0; i < mr.nMap; i++ {
+		var reply DoJobReply
+		workerAddr = <-mr.registerChannel
+		args := &DoJobArgs{
+			File:          mr.file,
+			Operation:     Map,
+			JobNumber:     i,
+			NumOtherPhase: mr.nReduce,
+		}
+		ok := call(workerAddr, "Worker.DoJob", args, &reply)
+		if ok == true {
+			mr.registerChannel <- workerAddr
+		}
+	}
+
+	fmt.Println("Map is done")
+
+	for i := 0; i < mr.nReduce; i++ {
+		var reply DoJobReply
+		workerAddr = <-mr.registerChannel
+		args := &DoJobArgs{
+			File:          mr.file,
+			Operation:     Reduce,
+			JobNumber:     i,
+			NumOtherPhase: mr.nMap,
+		}
+		ok := call(workerAddr, "Worker.DoJob", args, &reply)
+		if ok == true {
+			mr.registerChannel <- workerAddr
+		}
+	}
+
+	fmt.Println("Reduce is done")
 
 	return mr.KillWorkers()
 }
